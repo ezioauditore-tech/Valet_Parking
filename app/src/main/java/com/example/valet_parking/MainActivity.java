f00801af727d1390b62ecc6b5927a0a79a4ea720
ezioauditore-tech <sakthivelpillai25@gmail.com>
2022-05-10 23:01:22 +0530

    commit

---
+++ app/src/main/java/com/example/valet_parking/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.valet_parking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView forgetpass;
+    TextView signup;
+    Button signin;
+    EditText username,password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+
+        forgetpass=findViewById(R.id.forgetbtn);
+        signin=findViewById(R.id.signinbtn);
+        username=findViewById(R.id.userin);
+        password=findViewById(R.id.passin);
+        signup=findViewById(R.id.signup_click);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Signup.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(  username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Intent intent=new Intent(MainActivity.this,Mapview.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "check your credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/valet_parking/Signup.java
+++ app/src/main/java/com/example/valet_parking/Signup.java
@@ -2,13 +2,47 @@ package com.example.valet_parking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
-
+     EditText email,password,username,retype;
+     TextView signin;
+     Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        email=findViewById(R.id.emailup);
+        password=findViewById(R.id.passup);
+        username=findViewById(R.id.userup);
+        retype=findViewById(R.id.repassup);
+        signin=findViewById(R.id.signin_click);
+        signup=findViewById(R.id.signupbtn);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup.this,Mapview.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent intent=new Intent(Signup.this,MainActivity.class);
+                 startActivity(intent);
+                 finish();
+
+            }
+        });
+
+
     }
 }
