59afd9a742603550573c3e3112896df1df80f6eb
venkatesh-mpro <venkateshm.pro@gmail.com>
2022-05-17 22:39:58 +0530

    fbar

--- app/src/main/java/com/example/valet_parking/Drawer.java
+++ app/src/main/java/com/example/valet_parking/Drawer.java
@@ -26,13 +26,6 @@ private ActivityDrawerBinding binding;
      setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarDrawer.toolbar);
-        binding.appBarDrawer.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
